d1f8e24b84573b1af233f64bf91909ddf6fd2f8a
aliceech <chemodanova70@icloud.com>
2022-03-21 18:32:16 +0300

    practice3

---
+++ intentapp/src/main/java/ru/mirea/chemodanova/intentapp/MainActivity2.java
@@ -0,0 +1,21 @@
+package ru.mirea.chemodanova.intentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView tw= (TextView) findViewById(R.id.textView);
+
+        Intent intent = getIntent();
+        String string = intent.getStringExtra("message");
+        tw.setText(string);
+    }
+}
